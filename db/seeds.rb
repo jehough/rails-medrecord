b5894df5631e2552b05dc05109d18965cc9b1dd7
Jim <teachlife@outlook.com>
2019-09-15 13:54:05 -0400

    adds patient to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 Doctor.create(username: "drwho", password: "tardis", first_name: "No One", last_name: "Knows", admin: false)
 Doctor.create(username: "drsuess", password: "redfish", first_name: "Henry", last_name: "Spencer", admin: true)
 
-Patient.create(email:)
+Patient.create(email: "joe@sample.com", password: "password", first_name: "Joe", last_name: "Smith", birthday: "1999-10-10", allergies: "none")
