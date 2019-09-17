bc9182ae194516c0f36cc30e337312cd1ca25d82
Jim <teachlife@outlook.com>
2019-09-16 21:11:23 -0400

    adds login function to session controllers

--- app/controllers/doctor_sessions_controller.rb
+++ app/controllers/doctor_sessions_controller.rb
@@ -4,5 +4,16 @@ class DoctorSessionsController < ApplicationController
   end
 
   def create
+    if @doctor = Doctor.find_by(params[:username]) && @doctor.authenticate(params[:password])
+      session[:doctor_id] = @doctor.id
+      redirect_to doctors_path(@doctor)
+    else
+      flash[:alert] = "Incorrect Email/Password Combination"
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+
   end
 end
--- app/controllers/patient_sessions_controller.rb
+++ app/controllers/patient_sessions_controller.rb
@@ -1,3 +1,22 @@
 class PatientSessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    if @patient = Patient.find_by(params[:email]) && @patient.authenticate(params[:password])
+      session[:patient_id] = @patient.id
+      redirect_to patient_path(@patient)
+    else
+      flash[:alert] = "Incorrect Email/Password Combination"
+      redirect_to '/'
+    end
+
+
+
+  end
+
+  def destroy
+
+  end
 end
