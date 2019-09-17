e171c2671a6063cc3c980a899799ccd7a9efba9c
Jim <teachlife@outlook.com>
2019-09-16 20:29:17 -0400

    Adds doctor show page

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,7 @@ class Appointment < ApplicationRecord
 belongs_to :patient, optional: true
 belongs_to :doctor
 has_one :macro
+
+  def display_time
+    "#{self.date.strftime("%b %e, %Y")} #{self.time.strftime("%l:%M %P")}"
 end
