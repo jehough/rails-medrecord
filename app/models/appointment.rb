55c7332f728bc2fb4d9030000110d16d554f4c89
Jim <teachlife@outlook.com>
2019-09-16 22:09:01 -0400

    adds appointments to doctors home

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ belongs_to :patient, optional: true
 belongs_to :doctor
 has_one :macro
 
-scope :today -> {where("date == ?", Date.today)}
+scope :today, -> {where("date = ?", Date.today)}
 
   def display_time
     "#{self.date.strftime("%b %e, %Y")} #{self.time.strftime("%l:%M %P")}"
