6c22fe9c0640bbc98fa8f05e1f50af7ed0fee092
Jim <teachlife@outlook.com>
2019-09-16 20:20:42 -0400

    adds Doctor index page

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,8 @@ class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
   has_many :doc_scheds
+
+  def doc_name
+    "Dr. #{self.first_name} #{self.last_name}"
+  end
 end
