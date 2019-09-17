1c759e0350ec2ac87d78c7575227ae0f34f4617b
Jim <teachlife@outlook.com>
2019-09-16 21:00:22 -0400

    adds patient login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get '/doctor/login' => 'doctor_sessions#new'
   post '/doctor/login' => 'doctor_sessions#create'
   get '/doctor/logout' => 'doctor_sessions#destroy'
+  get '/patient/login' => 'patient_sessions#new'
+  post '/patient/login' => 'patient_sessions#create'
+  get '/patient/logout' => 'patient_sessions#destroy'
 
 end
