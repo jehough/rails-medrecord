0aaedea32a3e06b8665b7dd14aca444485fa9115
Jim <teachlife@outlook.com>
2019-09-17 20:33:47 -0400

    udpates appointments table and adds more seed data

--- db/migrate/20190915172943_create_appointments.rb
+++ db/migrate/20190915172943_create_appointments.rb
@@ -5,6 +5,27 @@ class CreateAppointments < ActiveRecord::Migration[5.2]
       t.belongs_to :doctor, foreign_key: true
       t.date :date
       t.time :time
+      t.integer :height
+      t.integer :weight
+      t.integer :bmi
+      t.integer :temp
+      t.integer :systolic
+      t.integer :diastolic
+      t.integer :heart_rate
+      t.integer :spo2
+      t.integer :rr
+      t.text :visit_reason
+      t.text :history
+      t.text :neuro
+      t.text :heent
+      t.text :cardiac
+      t.text :respiratory
+      t.text :skin
+      t.text :musculoskeletal
+      t.text :psychosocial
+      t.text :tests_ordered
+      t.text :therapies
+      t.text :follow_up
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@ Doctor.create(username: "drwho", password: "tardis", first_name: "No One", last_
 Doctor.create(username: "drsuess", password: "redfish", first_name: "Henry", last_name: "Spencer", admin: true)
 
 Patient.create(email: "joe@sample.com", password: "password", first_name: "Joe", last_name: "Smith", birthday: "1999-10-10", allergies: "none")
+
+@sched_1 = DocSched.create(date: Date.today, shift_start: "08:00:00", shift_end: "14:00:00", doctor_id: 1)
+@sched_2 = DocSched.create(date: Date.today, shift_start: "11:00:00", shift_end: "17:00:00", doctor_id: 2)
+
+@sched_1.make_appointments
+@sched_2.make_appointments
