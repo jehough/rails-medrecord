4ba18f0ee95d1a97d8c04fb48b4b71b527cc1775
Jim <teachlife@outlook.com>
2019-09-18 09:36:32 -0400

    adds update route and appointments params to appoint controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,4 +7,12 @@ class AppointmentsController < ApplicationController
   def edit
     @appointment = Appointment.find(params[:id])
   end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:height, :weight, :bmi, :temp, :systolic, :diastolic, :heart_rate, :spo2, :rr, :visit_reason, :history, :neuro, :heent, :cardiac, :respiratory, :skin, :musculoskeletal, :psychosocial, :therapies, :tests_ordered, :follow_up)
 end
