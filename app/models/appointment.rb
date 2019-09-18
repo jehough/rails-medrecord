97bee7a1dd5fca2604b1b77d38c86d0fa85d24a4
Jim <teachlife@outlook.com>
2019-09-17 20:37:08 -0400

    migrates database and adds scope to Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,6 +4,7 @@ belongs_to :doctor
 has_one :macro
 
 scope :today, -> {where("date = ?", Date.today)}
+scope :future, -> {where("date > ?", Date.today)}
 
   def display_time
     "#{self.date.strftime("%b %e, %Y")} #{self.time.strftime("%l:%M %P")}"
