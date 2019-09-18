a3243dedd99e7559ba79f1edbae503792a859ecc
Jim <teachlife@outlook.com>
2019-09-18 06:49:35 -0400

    adds table and form to appointments edit page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,8 @@ class AppointmentsController < ApplicationController
   def show
     @appointment = Appointment.find(params[:id])
   end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
 end
