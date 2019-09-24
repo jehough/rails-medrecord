9d59247e5876b1be1e7eb1164bd827747acde975
Jim <teachlife@outlook.com>
2019-09-23 22:43:44 -0400

    adds nested attributes for patient meds to appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,12 @@
 class Appointment < ApplicationRecord
-belongs_to :patient, optional: true
-belongs_to :doctor
+  belongs_to :patient, optional: true
+  belongs_to :doctor
+  accepts_nested_attributes_for :patient_meds
 
-
-scope :today, -> {where("date = ?", Date.today)}
-scope :future, -> {where("date >= ?", Date.today)}
-scope :available, -> {where("patient_id IS NULL")}
-scope :past, -> {where("date < ?", Date.today)}
+  scope :today, -> {where("date = ?", Date.today)}
+  scope :future, -> {where("date >= ?", Date.today)}
+  scope :available, -> {where("patient_id IS NULL")}
+  scope :past, -> {where("date < ?", Date.today)}
 
   def display_time
     "#{self.date.strftime("%b %e, %Y")} #{self.time.strftime("%l:%M %P")}"
